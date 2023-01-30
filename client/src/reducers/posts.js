b9f4715f2eb9c6935437626cd67cb357bf0a1a9f
Damian Wojtal <106908962+Wojtasko99@users.noreply.github.com>
2023-01-30 15:19:15 +0100

    Client files commit

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,18 @@
+import { FETCH_ALL, UPDATE, DELETE, LIKE, CREATE } from '../constants/actionTypes';
+
+export default (posts = [], action) => {
+    switch(action.type){
+        case FETCH_ALL:
+            return action.payload;
+        case CREATE:
+            return [...posts, action.payload];
+        case UPDATE:
+        case LIKE:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        case DELETE:
+            return posts.filter((post) => post._id !== action.payload);
+        case '':
+        default:
+            return posts;
+    }
+}
